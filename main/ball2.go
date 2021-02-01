e046f83b7e57179645ff677d5f7ba9869708e811
ngqhung0912 <ngqhung0912@gmail.com>
2021-02-01 16:56:28 +0700

    added 2nd player

--- main/ball2.go
+++ main/ball2.go
@@ -1,8 +1,23 @@
+components {
+  id: "ball_2"
+  component: "/scripts/ball_2.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/assets/assets.atlas\"\n"
-  "default_animation: \"bg\"\n"
+  "default_animation: \"ball_00\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
