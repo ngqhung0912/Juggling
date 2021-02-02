9b699f331668e69e850d44b9f14f01d933ea0b48
ngqhung0912 <ngqhung0912@gmail.com>
2021-02-02 17:59:27 +0700

    ui is partially functioning

--- main/controller.go
+++ main/controller.go
@@ -13,3 +13,54 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "main_menu"
+  component: "/main/main_menu/main_menu.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "score_menu_proxy"
+  type: "collectionproxy"
+  data: "collection: \"/main/score_menu/score_menu.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "game_proxy"
+  type: "collectionproxy"
+  data: "collection: \"/main/game/game.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
